1b833fcdd46b0329c9eaae4c5f405fe8f7c3c511
HansaSandamal <hansasandamal@gmail.com>
2021-05-20 20:55:38 +0530

    Create Header

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./layouts/Header";
+import Footer from "./layouts/Footer";
 
 
 function App() {
   return (
     <div className="App">
         <Header/>
+        <Footer/>
     </div>
   );
 }
