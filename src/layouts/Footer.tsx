976a5cc3541e1804f1077de117c9e47d1bbcc0e3
HansaSandamal <hansasandamal@gmail.com>
2021-05-20 21:00:51 +0530

    Create Footer

---
+++ src/layouts/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Container} from "react-bootstrap";
+
+const Footer : React.FC=()=>{
+    return(
+        <Container fluid={true} className="pt-3 h5 bg-dark text-white">
+            <span>Hansa Sandamal</span>
+
+        </Container>
+    )
+}
+export default Footer;
